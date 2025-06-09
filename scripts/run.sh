089169554121daca82b1ecc220d72d833f1edeb2
cnsivakumar <cnsivakumar@gmail.com>
2025-06-08 18:20:28 -0600

    test defaults

---
+++ scripts/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "-----------------------------------"
+echo "Hello from the sample shell script!"
+echo "Current date and time:"
+date
+
+echo "Current working directory:"
+pwd
+
+echo "Listing files in this directory:"
+ls -l
+
+echo "-----------------------------------"

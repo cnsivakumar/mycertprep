76c6ba7f3896394421a7794bebc2bc9b74ac7913
cnsivakumar <cnsivakumar@gmail.com>
2025-06-08 18:29:37 -0600

    push another changes to test concurrency

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,7 @@ echo "Hello from the sample shell script!"
 echo "Current date and time:"
 date
 
-echo "Current working directory:"
+echo "Current working directorys:"
 pwd
 
 echo "Listing files in this directory:"
